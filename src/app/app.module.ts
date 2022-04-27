04497121094c6fa06714852041ed0dadf93c204b
hudjoubert <hudson_joubert@yahoo.com.br>
2022-04-27 13:06:05 -0300

    create table component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { PesquisaComponent } from './pesquisa/pesquisa.component';
 import { HttpClientModule } from '@angular/common/http';
+import { ListaProdutoComponent } from './produto/lista-produto/lista-produto.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PesquisaComponent
+    PesquisaComponent,
+    ListaProdutoComponent
   ],
   imports: [
     BrowserModule,
