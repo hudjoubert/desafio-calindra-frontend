2fc4011fa95295eb8c9546f439a89cb849d1b43c
hudjoubert <hudson_joubert@yahoo.com.br>
2022-04-26 23:55:05 -0300

    create component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PesquisaComponent } from './pesquisa/pesquisa.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PesquisaComponent
   ],
   imports: [
     BrowserModule
